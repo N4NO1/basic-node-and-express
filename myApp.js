a6c9b33cc34e222ee45eebcc09be41d1dbfd9228
N4NO <nathan.rouse56734@gmail.com>
2022-03-05 14:48:23 +0000

    added body parsing

--- myApp.js
+++ myApp.js
@@ -1,12 +1,15 @@
+const bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 require('dotenv').config()
 console.log("Hello World")
+bodyParser = require('body-parser')
 
 app.use("/public", express.static(__dirname + "/public"))
 
 app.use("/", function (req, res, next) {
     console.log(req.method + " " + req.path + " - " + req.ip)
+    bodyParser.urlencoded({extended: false})
     next()
 })
 
@@ -41,6 +44,8 @@ app.get("/:word/echo", function (req, res) {
 
 app.route("/name").get(function (req, res) {
     res.json({"name": req.query.first + " " + req.query.last})
+}).post(function (req, res) {
+
 })
 
 
