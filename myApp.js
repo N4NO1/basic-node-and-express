4df2ddd2e8a343af8e222a655df01d36a9053949
Nathan Rouse <nathan.rouse56734@gmail.com>
2022-03-09 12:00:21 +0000

    added email form

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+
 
 console.log("Hello World")
 
 app.use("/public", express.static(__dirname + "/public"))
+app.use(bodyParser.urlencoded({extended: false}))
 
 
 app.get("/", function (req, res) {
@@ -15,6 +18,14 @@ app.get("/json", function (req, res) {
     res.json({"message": "Hello json"})
 })
 
+
+app.post("/name", function (req, res) {
+
+    res.json({
+        "name": req.body.first + " " + req.body.last,
+        "email": req.body.email
+    })
+})
 
 
 
